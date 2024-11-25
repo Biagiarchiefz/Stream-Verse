940c716ac7485f7b9fdf8d7e7733f09306093340
Biagiarchiefz <biagiiarchie@gmail.com>
2024-11-25 23:05:08 +0700

    push

--- stream-verse/src/Components/Footer/Footer.jsx
+++ stream-verse/src/Components/Footer/Footer.jsx
@@ -4,12 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="mt-20 mx-auto">
-        <h1>dadad</h1>
-        <h2>adadad
-          <h3>dadaada</h3>
-          <h4>wdwdwdwdw</h4>
-          <h1>ssfs</h1>
-        </h2>
+      
       </div>
     </footer>
   )
